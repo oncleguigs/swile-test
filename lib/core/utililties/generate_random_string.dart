91a6d62bf97cb24dc3bc4955a4b522c471beda08
Guillaume Vignolle <guillaume.vignolle@gmail.com>
2022-05-26 20:32:21 +0200

    core helpers functions

---
+++ lib/core/utililties/generate_random_string.dart
@@ -0,0 +1,8 @@
+import 'dart:math';
+
+String generateRandomString(int len) {
+  /// This function is used to generate a random string for unique hero ID
+  var r = Random();
+  const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+  return List.generate(len, (index) => _chars[r.nextInt(_chars.length)]).join();
+}
